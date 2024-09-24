ea0715e3fbeed4932ac88518b0233d0c8b46a4a2
Poojashree <poojashree8199@gmail.com>
2024-09-24 10:54:21 +0530

    first commit

---
+++ php-app/about.php
@@ -0,0 +1,136 @@
+<?php
+// Initialize the session
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'header.php'?>
+		<!-- Hero-area -->
+		<div class="hero-area section">
+
+			<!-- Backgound Image -->
+			<div class="bg-image bg-parallax overlay" style="background-image:url(./img/bgc2.jpg); " ></div>
+			<!-- /Backgound Image -->
+
+			<div class="container">
+				<div class="row" >
+					<div class="col-md-10 col-md-offset-1 text-center">
+						<ul class="hero-area-tree">
+							<li><a href="http://localhost/career/main.php">Home</a></li>
+							<li>Contact</li>
+						</ul>
+						<h1 class="white-text">About Us</h1>
+
+					</div>
+				</div>
+			</div>
+
+		</div>
+		<!-- /Hero-area -->
+
+		<!-- Contact -->
+		<div id="contact" class="section">
+
+			<!-- container -->
+			<div class="container">
+
+				<!-- row -->
+				<div class="row">
+
+                    <!-- About -->
+                    <div id="about" class="section">
+
+                    <!-- container -->
+                    <div class="container">
+
+                        <!-- row -->
+                        <div class="row">
+
+                            <div class="col-md-6">
+                                <div class="section-header">
+                                    <h2 style="font-size: 35px;">About Career System</h2>
+                                    <p class="lead" style="font-size: 18px; margin-top: 50px;">Education seekers get a personalised experience on our site, based on educational background and career interest, enabling them to make well informed course and career decisions. The decision making is empowered with easy access to detailed information on career choices, courses, exams, colleges, admission criteria, eligibility, placement statistics, rankings, reviews, scholarships, latest updates etc as well as by interacting with other career.ly users, experts, current students in colleges and alumni groups. We have introduced several student oriented products and tools like Career Prediction, Knowledge Network, Daily Bytes, Blogs, Community discussion forum, and various Courses.</p>
+                                    <!--Education seekers get a personalised experience on our site, based on educational background and career interest, enabling them to make well informed course and college decisions. The decision making is empowered with easy access to detailed information on career choices, courses, exams, colleges, admission criteria, eligibility, fees, placement statistics, rankings, reviews, scholarships, latest updates etc as well as by interacting with other Shiksha.com users, experts, current students in colleges and alumni groups. We have introduced several student oriented products and tools like Career Central, Common Application Form, Top Colleges, College Compare, Alumni Employment Stats, Campus Connect, College Reviews, College Predictors, MyShortlist and Shiksha Café.-->
+                                </div>
+
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="about-img">
+                                    <img src="./img/about.png" alt="">
+                                </div>
+                            </div>
+
+                        </div>
+                        <!-- row -->
+                        <hr class="section-hr">
+                    </div>
+                    <!-- container -->
+                    </div>
+                    <!-- /About -->
+                    </div>
+				<!-- /row -->
+
+			</div>
+			<!-- /container -->
+
+		</div>
+		<!-- /Contact -->
+
+		
+		<!-- Footer -->
+		<footer id="footer" class="section">
+
+			<!-- container -->
+			<div class="container">
+
+				<!-- row -->
+				<div class="row">
+
+					<!-- footer logo -->
+					<div class="col-md-6">
+						<div class="footer-logo">
+							<a class="logo" style="font-size: 30px;" href="main.html">Career System</a>
+						</div>
+					</div>
+					<!-- footer logo -->
+
+
+				</div>
+				<!-- /row -->
+
+				<!-- row -->
+				<div id="bottom-footer" class="row">
+
+					<!-- social -->
+					<div class="col-md-4 col-md-push-8">
+						
+					</div>
+					<!-- /social -->
+
+			
+
+				</div>
+				<!-- row -->
+
+			</div>
+			<!-- /container -->
+
+		</footer>
+		<!-- /Footer -->
+		
+		<!-- preloader -->
+		<div id='preloader'><div class='preloader'></div></div>
+		<!-- /preloader -->
+
+
+		<!-- jQuery Plugins -->
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+		<script type="text/javascript" src="js/google-map.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+
+	</body>
+</html>
---
+++ php-app/footer.php
@@ -0,0 +1,40 @@
+		<!-- Footer -->
+		<footer id="footer" class="section" >
+
+			<!-- container -->
+			<div class="container" >
+
+				<!-- row -->
+				<div class="row">
+
+					<!-- footer logo -->
+					<div class="col-md-6" >
+						<div class="footer-logo" >
+							<a class="logo" style="font-size: 30px; " href="main.php">Career System</a>
+						</div>
+					</div>
+					<!-- footer logo -->
+
+
+				</div>
+				<!-- /row -->
+
+				
+
+			</div>
+			<!-- /container -->
+
+		</footer>
+		<!-- /Footer -->
+
+		<!-- preloader -->
+		<div id='preloader'><div class='preloader'></div></div>
+		<!-- /preloader -->
+
+
+		<!-- jQuery Plugins -->
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+
+    </body>
